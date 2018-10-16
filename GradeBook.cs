c1586d3f36f6342a4c75c58cf8e3b78c5f5f650c
Willian Miyasaka <wkmiyasaka@gmail.com>
2018-10-16 10:33:54 -0300

    Create GradeBook.cs

---
+++ GradeBook.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+
+        public GradeBook()
+        {
+
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades = new List<float>();
+
+    }
+}
