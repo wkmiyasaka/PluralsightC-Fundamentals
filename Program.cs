05010198d4ca96d7575e7fffe90386f8dab274d3
Willian Miyasaka <wkmiyasaka@gmail.com>
2018-10-16 12:02:00 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,10 +13,13 @@ namespace Grades
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
+            book.AddGrade(75f);
 
-            //GradeBook book2 = new GradeBook();
-            GradeBook book2 = book;
-            book2.AddGrade(75);
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            
         }
     }
 }
