992faca06b3f945464d0a90dd744b2a294fac4cb
Willian Miyasaka <wkmiyasaka@gmail.com>
2018-10-16 12:01:10 -0300

    Update GradeBook.cs

--- GradeBook.cs
+++ GradeBook.cs
@@ -14,12 +14,43 @@ namespace Grades
             grades = new List<float>();
         }
 
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            
+
+            float sum = 0;
+            if (grades.Count > 0)
+            {
+                foreach (float grade in grades)
+                {
+                    /*
+                    if (grade > stats.HighestGrade)
+                    {
+                        stats.HighestGrade = grade;
+                    }
+
+                    if (grade < stats.LowestGrade)
+                    {
+                        stats.LowestGrade = grade;
+                    }
+                     */
+                    stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                    stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                    sum += grade;
+                }
+                stats.AverageGrade = sum / grades.Count;
+            }
+            
+            return stats;
+        }
+
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
-        List<float> grades;
+        private List<float> grades;
 
     }
 }
