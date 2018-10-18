8364d96e65e74ab72ee334d6ce183846f0c7c740
Willian Miyasaka <wkmiyasaka@gmail.com>
2018-10-18 11:07:05 -0300

    Create NameChangedEventArgs.cs

---
+++ NameChangedEventArgs.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class NameChangedEventArgs : EventArgs
+    {
+        public string ExistingName { get; set; }
+        public string newName { get; set; }
+    }
+}
